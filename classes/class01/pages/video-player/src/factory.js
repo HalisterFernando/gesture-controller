c6e2d44c66ada9c1571bbe110f66e8872a6a5284
Halister Fernando dos Santos <halisterfernando@hotmail.com>
2023-02-27 23:03:42 -0300

    suporte ao firefox

--- classes/class01/pages/video-player/src/factory.js
+++ classes/class01/pages/video-player/src/factory.js
@@ -2,7 +2,27 @@ import Controller from './controller.js'
 import Service from './service.js'
 import View from './view.js'
 import Camera from '../../../lib/shared/camera.js'
+import { supportsWorkerType } from '../../../lib/shared/util.js'
 
+async function getWorker() {
+  if (supportsWorkerType()) {
+    console.log('suporta')
+    const worker = new Worker('./src/worker.js', {type: 'module'})
+    return worker;
+  } 
+
+  const workerMock = {
+    async postMessage() {},
+    onmessage(msg) {}
+  }
+    
+  console.log('não suporta')
+  return workerMock;
+  
+}
+
+const worker = await getWorker()
+worker.postMessage('hey from factory')
 const camera = await Camera.init()
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
